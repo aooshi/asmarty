38a37c9c862a4aaddcfcf1ea333695e00b144576
xiaobogit <lxb@aooshi.org>
2019-10-15 22:10:34 +0800

    TestApplication.csproj

---
+++ TestApplication/Program.cs
@@ -0,0 +1,42 @@
+﻿using Adf;
+using ASmarty.ViewEngine;
+using System;
+using System.Net;
+
+namespace TestApplication
+{
+    class Program
+    {
+        public static ViewEngine ViewEngine
+        {
+            get;
+            private set;
+        }
+
+        static void Main(string[] args)
+        {
+            ViewConfiguration vc = new ViewConfiguration();
+
+            vc.ApplicationPath = "/";
+            vc.Caching = true;
+            //vc.PluginFolder = "";
+            vc.ViewRootPath = System.IO.Path.Combine(AppContext.BaseDirectory, "Views/");
+            vc.ViewExtension = ".tpl";
+
+            //
+            ViewEngine = new ViewEngine(vc);
+
+
+            Adf.HttpServer server = new Adf.HttpServer(8082);
+            server.Callback = HttpServerCallback;
+            server.Start();
+        }
+
+        private static HttpStatusCode HttpServerCallback(HttpServerContext context)
+        {
+            context.Content = "23";
+
+            return HttpStatusCode.OK;
+        }
+    }
+}
