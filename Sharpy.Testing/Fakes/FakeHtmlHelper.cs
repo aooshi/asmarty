1fc0490b1a2f333240cdf8e2e683768c4d93101f
xiaobogit <lxb@aooshi.org>
2019-10-15 18:15:37 +0800

    init

--- Sharpy.Testing/Fakes/FakeHtmlHelper.cs
+++ Sharpy.Testing/Fakes/FakeHtmlHelper.cs
@@ -1,20 +1,20 @@
-﻿using System.Web.Mvc;
-using System.Web.SessionState;
+﻿//using System.Web.Mvc;
+//using System.Web.SessionState;
 
-namespace Sharpy.Testing.Fakes
-{
-    public class FakeHtmlHelper : HtmlHelper
-    {
-        public FakeHtmlHelper() : this(new FakeController(), new FakeView())
-        {
-        }
+//namespace Sharpy.Testing.Fakes
+//{
+//    public class FakeHtmlHelper : HtmlHelper
+//    {
+//        public FakeHtmlHelper() : this(new FakeController(), new FakeView())
+//        {
+//        }
 
-        public FakeHtmlHelper(ControllerBase controller, IView view) : this(controller, view, new SessionStateItemCollection())
-        {
-        }
+//        public FakeHtmlHelper(ControllerBase controller, IView view) : this(controller, view, new SessionStateItemCollection())
+//        {
+//        }
 
-        public FakeHtmlHelper(ControllerBase controller, IView view, SessionStateItemCollection sessionData) : base(new FakeViewContext(controller, view, sessionData), new FakeViewDataContainer())
-        {
-        }
-    }
-}
+//        public FakeHtmlHelper(ControllerBase controller, IView view, SessionStateItemCollection sessionData) : base(new FakeViewContext(controller, view, sessionData), new FakeViewDataContainer())
+//        {
+//        }
+//    }
+//}
--- Sharpy.Testing/Fakes/FakeViewContext.cs
+++ Sharpy.Testing/Fakes/FakeViewContext.cs
@@ -1,12 +1,12 @@
-﻿using System.Web.Mvc;
-using System.Web.SessionState;
+﻿//using System.Web.Mvc;
+//using System.Web.SessionState;
 
-namespace Sharpy.Testing.Fakes
-{
-    public class FakeViewContext : ViewContext
-    {
-        public FakeViewContext(ControllerBase controller, IView view, SessionStateItemCollection sessionData) : base(new FakeControllerContext(controller, sessionData), view, new ViewDataDictionary(), new TempDataDictionary())
-        {
-        }
-    }
-}
+//namespace Sharpy.Testing.Fakes
+//{
+//    public class FakeViewContext : ViewContext
+//    {
+//        public FakeViewContext(ControllerBase controller, IView view, SessionStateItemCollection sessionData) : base(new FakeControllerContext(controller, sessionData), view, new ViewDataDictionary(), new TempDataDictionary())
+//        {
+//        }
+//    }
+//}
